e37f7fde626797bd13a6523ae146f93b4b084265
John Tutchings <jtutchings@users.noreply.github.com>
2014-08-12 13:19:36 +0100

    Update access.php

--- teacherlinks/db/access.php
+++ teacherlinks/db/access.php
@@ -14,10 +14,41 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * teacherlinks block caps.
+ *
+ * @package    block_teacherlinks
+ * @copyright  John Tutchings <j.tutchings@coventry.ac.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
 
+    'block/teacherlinks:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+
+    'block/teacherlinks:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+    
     'block/teacherlinks:viewlink' => array(
         'captype' => 'read',
         'contextlevel' => CONTEXT_BLOCK,
@@ -31,5 +62,4 @@ $capabilities = array(
         ),
 
     ),
-
 );
